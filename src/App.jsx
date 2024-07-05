91392f50e745c8e56d488904d0b79b304c75b347
BSenaS <not.today.bs@gmail.com>
2024-07-05 15:04:11 +0300

    Creates Routing and Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import { HomePage } from "./pages/HomePage";
+import { MoviesPage } from "./pages/MoviesPage";
+import { MovieDetailPage } from "./pages/MovieDetailPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/movies",
+    element: <MoviesPage />,
+  },
+  {
+    path: "/movie",
+    element: <MovieDetailPage />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <p>Hello</p>
+      <RouterProvider router={router} />
     </>
   );
 }
